74bb373c20e0bf166f70f3b3551ee8b0eb1f6c2a
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2022-06-23 16:07:44 -0300

    corrigindo bugs

--- assets/js/modules/scriptsPaginas/index.js
+++ assets/js/modules/scriptsPaginas/index.js
@@ -6,9 +6,7 @@ const btnForm = document.getElementById('btn-grid1');
 function activeForm(){
     
     const sectionOpen = document.getElementById('grid-2');
-    const sectionClose = document.getElementById('grid-1');
     sectionOpen.classList.remove('active');
-    sectionClose.classList.add('close');
 }
 
 btnForm.addEventListener('click', activeForm);
